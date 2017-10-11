5064e3959dc59e20a5a26de44d0ee4f70d982b71
lucasdpn <lucasdpnunes@gmail.com>
2017-10-11 14:27:31 -0300

    setting environment; creation of userquery view

---
+++ readme.txt
@@ -0,0 +1,7 @@
+# create environment
+cd PROJECT_DIR
+virtualenv --python=`which python3` ~/env
+pip install -r requirements.txt
+
+
+
---
+++ user_query/userquery/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+
+
+class UserQuery(models.Model):
+    name = models.CharField(
+        max_length=128, verbose_name='Name')
+    description = models.CharField(
+        max_length=1024, null=True, blank=True, verbose_name='Description')
+    query = models.CharField(
+        max_length=4096, null=False, blank=True, verbose_name='Query')
+    creationdate = models.DateTimeField(
+        auto_now_add=True, null=True, blank=True, verbose_name='Date', help_text='Creation Date')
+    tablename = models.CharField(
+        max_length=128, null=False, blank=True, verbose_name='Table Name')
+    is_public = models.BooleanField(
+        default=False, verbose_name='Is Public', help_text='Is Public default True')
+
+    def __str__(self):
+        return self.name
